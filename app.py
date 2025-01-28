5e3dac3231672256b106e92b1fa5ef48851efe09
Dionisius951 <dionisius678@gmail.com>
2025-01-28 18:28:21 +0700

    update app

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def handle_frame(image_data):
         print(f"Error processing frame: {e}")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
