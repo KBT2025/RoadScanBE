415a1b630b27e39b976f3ad6faf421039931c504
Dionisius951 <dionisius678@gmail.com>
2025-01-28 23:07:16 +0700

    fix app

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
